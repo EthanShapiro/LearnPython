37bd9e4bfe7d99b3c952e1bf3473e1d46bf3e9bb
EthanShapiro <ethanshapiro123@gmail.com>
2017-04-14 11:40:54 -0700

    ...

--- Challenges/3 List Less Than Ten.py
+++ Challenges/3 List Less Than Ten.py
@@ -1,1 +1,8 @@
 numList = [1, 6, 8, 9, 200, 88, 99, 10, 4, 3, 5]
+numbers = []
+
+for i in numList:
+    if i < 5:
+        numbers.append(i)
+
+print(numbers)
---
+++ Challenges/4 Divisors.py
@@ -0,0 +1,6 @@
+numbers = range(2, 11)
+divisors = []
+dividend = input("Please input the number you would like to see divisors for: ")
+for i in numbers:
+    if dividend / i == 0:
+        divisors.append()
