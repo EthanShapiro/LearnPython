c71387053e307d958a02d958140c08e3728b6220
EthanShapiro <ethanshapiro123@gmail.com>
2017-04-29 16:43:43 -0700

    ...

---
+++ Extra Resources/Lists.py
@@ -0,0 +1,17 @@
+# List Declarations
+stack = [1, 2, 3]
+stack1 = list([1, 2, 3])
+
+# Adding to the end
+stack.append(4)
+stack.append(5)
+
+print(stack)
+
+# Removing the last item
+stack.pop()
+print(stack)
+
+# Removing an item at a specified index
+stack.pop(0)
+print(stack)
